5c2a9363d129e56607394115f8cd25892fb85341
Ahmed Thaer Khalaf <48158716+Ahmed-Thaer-Khalaf@users.noreply.github.com>
2023-04-17 16:53:45 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+export const data = {
+
+    message: 'Hello Vue!',
+    number: '',
+    baseFrom : "10",
+    baseTo : "2",
+    image: '',
+    tage: '10',
+    options : {
+        backgroundColor	: "#ffffff00", 
+        scale : window.devicePixelRatio ,
+        
+    },
+
+    backgroundColor:{
+        hex: "#ff0000",
+        binary:"#00ff00",
+        decimal: "#0000ff",
+    },
+
+    solved : [] ,
+    
+    fruits: ['Binary','Octal','Hex'],
+      selectedFruit: 'Binary',
+    
+};
